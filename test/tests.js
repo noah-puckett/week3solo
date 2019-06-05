09ce66f2d091595f5363efa7de48ec8488efe7d5
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-05 16:28:29 -0700

    added new test

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@ import './html-equal.js';
 // import utilities and custom assertions
 import './Profile.test.js';
 import { app } from '../src/services/firebase.js'; 
+import './RoomList.test.js';
 
 QUnit.done(() => {
     app.delete();
