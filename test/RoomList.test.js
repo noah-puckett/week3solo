9e3ad697358c6d3c43054ba72f2f0b391e462367
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-05 16:30:17 -0700

    created test and added button for deleting rooms

---
+++ test/RoomList.test.js
@@ -0,0 +1,30 @@
+//TODO: fix imports with testing file, rename this file
+import RoomItem from '../src/chat/RoomItem.js';
+
+const test = QUnit.test;
+
+QUnit.module('Profile Component');
+
+
+test('Can my dumb room ITEM return something DYNAMICALLY?', assert => {
+    // arrange
+    const room = {
+        key: '-LgdScWdzi4YwpxDYSM3',
+        name: 'Test Room'
+    };
+
+    const roomItem = new RoomItem({ room });
+
+    const expected = /*html*/`
+        <li>
+            <a href="./chat.html?key=${room.key}">${room.name}</a>
+            <button>Delete Room</button>
+        </li>
+    `;
+
+    // act
+    const rendered = roomItem.renderTemplate();
+
+    // assert
+    assert.htmlEqual(rendered, expected);
+});
