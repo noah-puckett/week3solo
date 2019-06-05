4a159fd758206cd4cb3249ca9934154c77318adc
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-05 16:23:14 -0700

    deleted shenanigans

--- src/chat/chat.js
+++
@@ -1,6 +0,0 @@
-import '../utils/check-auth.js';
-import ChatApp from './ChatApp.js';
-
-const root = document.getElementById('chat');
-const chatApp = new ChatApp();
-root.appendChild(chatApp.render());
